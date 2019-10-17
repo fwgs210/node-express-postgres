1083d7390b2714af9b0bde8eca81ef9f68387432
tracy su <tracysu@tracys-MacBook-Pro.local>
2019-10-17 10:15:31 -0400

    fix swagger

--- src/swaggerApi/index.js
+++ src/swaggerApi/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./src/swaggerApi/documentation.yaml');
+const swaggerDocument = YAML.load('./public/documentation.yaml');
  
 router.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
