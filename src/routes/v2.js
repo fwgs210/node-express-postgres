8cb841608b00748c47334cdd33b56603d1c54769
Tracy <tracy.su@innovapost.com>
2019-10-31 14:42:51 -0400

    clean up

--- src/routes/v2.js
+++ src/routes/v2.js
@@ -2,11 +2,6 @@ const express = require('express')
 const passport    = require('passport');
 const router = express.Router()
 const chatController = require('../controllers/chatController')
-const categoryController = require('../controllers/categoryController')
-const adminController = require('../controllers/adminController')
-const authController = require('../controllers/authController')
-const jwt = require('../middleware/auth')
-const hashPassword = require('../middleware/hashPassword')
 
 router.route('/')
     .get((req, res) => res.send('api v2 is up'))
@@ -15,13 +10,18 @@ router.route('/')
 router.route('/chatroom/:chatroomId')
     .get(chatController.getChatroomChats)
 
-router.route('/chatroom/user/:userId')
-    .get(chatController.getUserChatrooms)
+router.route('/chatroom/user/:userId').get(
+    chatController.getUserChatrooms
+)
 
-router.route('/chatroom/create')
-    .post(chatController.createChatroom)
+router.route('/chatroom/create').post(
+    passport.authenticate('jwt', {session: false}),
+    chatController.createChatroom
+)
 
-router.route('/chatroom/chat')
-    .post(chatController.createChat)
+router.route('/chatroom/chat').post(
+    passport.authenticate('jwt', {session: false}), 
+    chatController.createChat
+)
 
 module.exports = router
